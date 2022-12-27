7dd08f1e3455bb2359a843e5ee8ae3b71b2a868b
LCH6890 <lorenachamorroalameda@gmail.com>
2022-12-27 18:17:05 +0100

    Se añade ejercicio 2

---
+++ Ejercicio 2.js
@@ -0,0 +1,10 @@
+//Ejercicio 2 con Quokka
+const lista = ["Lorena", 32, true, 06-08-1990]
+const libro = { 
+    nombre: "Harry Potter y la piedra filosofal",
+    autor: "J.K. Rowlin",
+    date: "1 de Diciembre de 1998",
+    "url": "https://www.amazon.es/nube-Mart%C3%ADn-Harry-Potter/dp/8498388872/ref=asc_df_8498388872/?tag=googshopes-21&linkCode=df0&hvadid=300782696631&hvpos=&hvnetw=g&hvrand=2761010680235304464&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=1005545&hvtargid=pla-587190369052&psc=1",
+}
+console.log(lista)
+console.log(libro)
